0cb09745350d58b4f86476dc15e43feccce0322c
ShyanJMC <shyanjmc@protonmail.com>
2022-10-23 04:24:44 -0300

    search - Added copyright note.

--- search/src/main.rs
+++ search/src/main.rs
@@ -1,3 +1,17 @@
+//! This file is part of RavnOS.
+//!
+//! RavnOS is free software: 
+//! you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+//! either version 3 of the License, or (at your option) any later version.
+//!
+//! RavnOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+//! without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//!
+//!  You should have received a copy of the GNU General Public License along with RavnOS. If not, see <https://www.gnu.org/licenses/>
+
+//!
+//! Copyright; Joaquin "ShyanJMC" Crespo - 2022
+
 // Standard libraries
 
 // Environment lib
