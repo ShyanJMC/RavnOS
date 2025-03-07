8ef334fdea6c48e7e63138e54366bc6add3fdbfb
Joaquin ShyanJMC Crespo <shyan@shyanjmc.com>
2025-03-07 03:20:44 -0300

    RavnOS kernel - Better build script now using logs directory

--- source_code/kernel/build.sh
+++ source_code/kernel/build.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
+kernel=kernel8.img
 
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied"
     echo -e "Arguments/Supported boards;\n rpi4"
     exit
-fi
-
-
-if [ $1 == "rpi4" ]; then
-	kernel=kernel8.img
-	board="bsp_rpi4"
-	cpuboard="cortex-a72"
-	libpath=$(pwd)/src/bsp/raspberrypi	
 fi
 
 echo "Cleaning temp files and backuping kernel8.img file"
@@ -27,9 +20,16 @@ echo "Verifing target and adding if not exist"
 rustup target add aarch64-unknown-none-softfloat
 	
 echo "Compiling"
-cargo rustc --target aarch64-unknown-none-softfloat --features $board --release -- -C target-cpu=$cpuboard -C link-arg=--library-path=$libpath -C link-arg=--script=$libpath/kernel.ld > logs/kernel-output.log 2>logs/kernel-error_output.log
+if [ $1 == "rpi4" ]; then	
+	board="bsp_rpi4"
+	cpuboard="cortex-a72"
+	libpath=$(pwd)/src/bsp/raspberrypi	
+	cargo rustc --target aarch64-unknown-none-softfloat --features $board --release -- -C target-cpu=$cpuboard -C link-arg=--library-path=$libpath -C link-arg=--script=$libpath/kernel.ld > logs/kernel-output.log 2>logs/kernel-error_output.log
+	
+fi
 	
 if [ $? -eq 0 ];then
+
 	echo "Copying object and stripping"
 	mkdir cargo-tmp
 	CARGO_TARGET_DIR=cargo-tmp/ cargo install cargo-binutils >logs/binutils-output.log 2>logs/bintuils-error_output.log
@@ -47,6 +47,9 @@ if [ $? -eq 0 ];then
 	echo "Creating in ../target/doc the documentation"
 	cargo doc --target aarch64-unknown-none-softfloat >/dev/null 2>/dev/null
 	echo -e "[0] Insert your SDCard\n[1] Copy kernel8.img and files in firmware/raspberry_pi-4/ into your SDCard.\n[2] Unplug it and boot"
+
+else 
+	echo "Failed compiling kernel, verify logs in logs/kernel-*.log file."
 fi
 
 echo "Restaring rust toolchain to stable"
