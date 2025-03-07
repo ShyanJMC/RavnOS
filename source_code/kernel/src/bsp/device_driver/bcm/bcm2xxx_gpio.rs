63e29d73ff86cde6baee83a5871cffdc42a8f611
Joaquin ShyanJMC Crespo <shyan@shyanjmc.com>
2025-03-07 03:29:49 -0300

    RavnOS kernel - Cleaned RPI 3 support

--- source_code/kernel/src/bsp/device_driver/bcm/bcm2xxx_gpio.rs
+++ source_code/kernel/src/bsp/device_driver/bcm/bcm2xxx_gpio.rs
@@ -139,33 +139,6 @@ impl GPIOInner {
     }
 
     /// Disable pull-up/down on pins 14 and 15.
-    #[cfg(feature = "bsp_rpi3")]
-    fn disable_pud_14_15_bcm2837(&mut self) {
-        use crate::cpu;
-
-        // Make an educated guess for a good delay value (Sequence described in the BCM2837
-        // peripherals PDF).
-        //
-        // - According to Wikipedia, the fastest RPi4 clocks around 1.5 GHz.
-        // - The Linux 2837 GPIO driver waits 1 µs between the steps.
-        //
-        // So lets try to be on the safe side and default to 2000 cycles, which would equal 1 µs
-        // would the CPU be clocked at 2 GHz.
-        const DELAY: usize = 2000;
-
-        self.registers.GPPUD.write(GPPUD::PUD::Off);
-        cpu::spin_for_cycles(DELAY);
-
-        self.registers
-            .GPPUDCLK0
-            .write(GPPUDCLK0::PUDCLK15::AssertClock + GPPUDCLK0::PUDCLK14::AssertClock);
-        cpu::spin_for_cycles(DELAY);
-
-        self.registers.GPPUD.write(GPPUD::PUD::Off);
-        self.registers.GPPUDCLK0.set(0);
-    }
-
-    /// Disable pull-up/down on pins 14 and 15.
     #[cfg(feature = "bsp_rpi4")]
     fn disable_pud_14_15_bcm2711(&mut self) {
         self.registers.GPIO_PUP_PDN_CNTRL_REG0.write(
@@ -185,8 +158,6 @@ impl GPIOInner {
             .modify(GPFSEL1::FSEL15::AltFunc0 + GPFSEL1::FSEL14::AltFunc0);
 
         // Disable pull-up/down on pins 14 and 15.
-        #[cfg(feature = "bsp_rpi3")]
-        self.disable_pud_14_15_bcm2837();
 
         #[cfg(feature = "bsp_rpi4")]
         self.disable_pud_14_15_bcm2711();
