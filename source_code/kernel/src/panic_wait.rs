8a23bd249a3b0403c735325c5eec16a8dc037d83
Joaquin ShyanJMC Crespo <shyan@shyanjmc.com>
2026-01-11 01:32:14 -0300

    panic: route handler output through UART

--- source_code/kernel/src/panic_wait.rs
+++ source_code/kernel/src/panic_wait.rs
@@ -6,16 +6,14 @@
 
 use core::panic::PanicInfo;
 
-// Maybe you are asking; why are you importin macro println! if it
+// Maybe you are asking; why are you importin macro uart_println! if it
 // is defined in console/mod.rs ? Not should be first "use crate::console"
-// and then the macro is automatically imported? 
+// and then the macro is automatically imported?
 // Well, not. Because when is imported in main.rs as module "mod console.rs",
 // is enabled as global macro in the hole program as root crate, and because
-// of that you just need use "use crate::println".
+// of that you just need use "use crate::uart_println".
 // Remember that this is because is a macro, is not the same behaviour in functions.
-use crate::println;
-
-use crate::cpu;
+use crate::{cpu, uart_println};
 
 //--------------------------------------------------------------------------------------------------
 // Private Code
@@ -57,7 +55,7 @@ fn panic(info: &PanicInfo) -> ! {
         _ => ("???", 0, 0),
     };
 
-    println!(
+    uart_println!(
         "Kernel panic!\n\n\
         Panic location:\n      File '{}', line {}, column {}\n\n\
         {}",
