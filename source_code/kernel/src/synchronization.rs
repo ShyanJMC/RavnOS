9aa11ea1a07076a057073c59a848dc214dda7e12
Joaquin ShyanJMC Crespo <shyan@shyanjmc.com>
2026-01-11 01:32:25 -0300

    sync: tidy NullLock documentation formatting

--- source_code/kernel/src/synchronization.rs
+++ source_code/kernel/src/synchronization.rs
@@ -3,10 +3,10 @@
 /// # UnsafeCell<T> from core lib
 /// The core primitive for interior mutability in Rust.
 ///
-/// If you have a reference &T, then normally in Rust the compiler performs optimizations based on 
-/// the knowledge that &T points to immutable data. Mutating that data, for example through an alias 
-/// or by transmuting an &T into an &mut T, is considered undefined behavior. UnsafeCell<T> opts-out 
-/// of the immutability guarantee for &T: a shared reference &UnsafeCell<T> may point to data that is being mutated. 
+/// If you have a reference &T, then normally in Rust the compiler performs optimizations based on
+/// the knowledge that &T points to immutable data. Mutating that data, for example through an alias
+/// or by transmuting an &T into an &mut T, is considered undefined behavior. UnsafeCell<T> opts-out
+/// of the immutability guarantee for &T: a shared reference &UnsafeCell<T> may point to data that is being mutated.
 /// This is called “interior mutability”.
 ///
 /// The first entry into CoreCell<T> is used with "[X].into()">
@@ -15,21 +15,21 @@ use core::cell::UnsafeCell;
 /// # Synchronization interfaces.
 /// ## Embedded mod
 pub mod interface {
-/// Any object implementing this trait guarantees exclusive access to the data wrapped within
-/// the Mutex for the duration of the provided closure.
+    /// Any object implementing this trait guarantees exclusive access to the data wrapped within
+    /// the Mutex for the duration of the provided closure.
     pub trait Mutex {
-/// The type of the data that is wrapped by this mutex.
-/// As this is just the signature, is not specified yet to which data is wrapped.
+        /// The type of the data that is wrapped by this mutex.
+        /// As this is just the signature, is not specified yet to which data is wrapped.
         type Data;
 
-/// Locks the mutex and grants the closure temporary mutable access to the wrapped data.
+        /// Locks the mutex and grants the closure temporary mutable access to the wrapped data.
         fn lock<'a, R>(&'a self, f: impl FnOnce(&'a mut Self::Data) -> R) -> R;
     }
 }
 
 /// A pseudo-lock for teaching purposes.
 ///
-/// In contrast to a real Mutex implementation, does not protect against 
+/// In contrast to a real Mutex implementation, does not protect against
 /// concurrent access from other cores to the contained data.
 ///
 /// The lock will only be used as long as it is safe to do so, i.e. as long as the kernel is
@@ -44,7 +44,7 @@ where
 }
 
 /// Send type: when it is "safe" to send it to another thread.
-///		Send is used main for object which need used (accessed) 
+///		Send is used main for object which need used (accessed)
 ///		between threads ONLY AT ONCE.
 ///		Becuase Rust ownership model, when "A" thread send the object
 ///		to "B", "A" loss the ownership over it.
@@ -63,7 +63,7 @@ unsafe impl<T> Send for NullLock<T> where T: ?Sized + Send {}
 unsafe impl<T> Sync for NullLock<T> where T: ?Sized + Send {}
 
 impl<T> NullLock<T> {
-/// Create an instance.
+    /// Create an instance.
     pub const fn new(data: T) -> Self {
         Self {
             data: UnsafeCell::new(data),
